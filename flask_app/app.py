66952b139cf982c24889fbff0b9f8d2d3fcbb139
Asish-T-Babu <asishtbabu@gmail.com>
2024-07-13 23:45:12 +0530

    Initial commit

---
+++ flask_app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Product(db.Model):
+    __tablename__ = "products"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    def __repr__(self):
+        return f"<Product {self.name}>"
+@app.route("/")
+def index():
+    return "<h1>Hello, World!</h1>"
